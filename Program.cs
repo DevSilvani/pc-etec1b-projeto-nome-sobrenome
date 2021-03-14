10aca1da44e232f34502148341b10925d5a3bb84
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-14 20:32:38 -0300

    C#

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_nome_sobrenome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome = "", sobrenome = "";
+            Console.Write("Insira seu nome: ");
+            nome = Console.ReadLine();
+            Console.Clear();
+            Console.Write("Insira seu sobrenome: ");
+            sobrenome = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Nome completo: "+nome+" "+sobrenome);
+            Console.WriteLine("Nome Catálogo: "+sobrenome+", "+nome);
+            Console.ReadKey();
+            
+
+
+        }
+    }
+}
